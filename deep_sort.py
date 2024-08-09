c9714f4696f567b3bbf2442ab927448781b2e793
Ante Kuic <ante.kuic2001@gmail.com>
2024-08-09 17:22:30 +0200

    small improvments!

--- deep_sort.py
+++ deep_sort.py
@@ -1,7 +1,8 @@
 import cv2
-import argparse
 from ultralytics import YOLO
 from deep_sort_realtime.deepsort_tracker import DeepSort
+import argparse
+import os
 
 # Postavljanje argument parsera
 parser = argparse.ArgumentParser(description="YOLOv8 i DEEP SORT praćenje")
@@ -9,6 +10,11 @@ parser.add_argument('--model', type=str, required=True, help="Putanja do YOLOv8 
 parser.add_argument('--video', type=str, required=True, help="Putanja do video datoteke")
 parser.add_argument('--output', type=str, default='output_video.avi', help="Putanja do izlaznog video zapisa")
 args = parser.parse_args()
+
+# Provjera da li video datoteka postoji
+if not os.path.exists(args.video):
+    print(f"Video datoteka '{args.video}' ne postoji.")
+    exit()
 
 # Uvezi YOLO model
 model = YOLO(args.model)
@@ -19,14 +25,27 @@ tracker = DeepSort(max_age=30, n_init=3, nn_budget=100)
 # Otvori video stream ili video datoteku
 cap = cv2.VideoCapture(args.video)
 
-# Postavljanje video writera
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(args.output, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+# Provjera da li je video uspješno otvoren
+if not cap.isOpened():
+    print(f"Neuspješno otvaranje video datoteke '{args.video}'.")
+    exit()
 
-while cap.isOpened():
+# Postavljanje video writera
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+out = cv2.VideoWriter(args.output, fourcc, 30.0, (frame_width, frame_height))
+
+while True:
     ret, frame = cap.read()
     if not ret:
+        # Ako nema više okvira, prekinuti petlju
+        print("Završeno čitanje okvira iz videa ili greška pri čitanju.")
         break
+
+    if frame is None or frame.size == 0:
+        print("Učitani okvir je prazan ili neispravan")
+        continue
     
     # Detekcija objekata pomoću YOLOv8
     results = model(frame)
@@ -51,7 +70,10 @@ while cap.isOpened():
         cv2.putText(frame, f'ID: {track_id}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     
     # Spremi frame u video
-    out.write(frame)
+    if frame.size > 0:
+        out.write(frame)
+    else:
+        print("Frame je neispravan za pisanje.")
 
 # Oslobađanje resursa
 cap.release()
